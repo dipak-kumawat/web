fb259a8ae866bfc1dbf13b213fff10840e2e05af
dipak-kumawat <dipaksk11@gmail.com>
2023-12-17 02:19:07 +0530

    electricity bill

---
+++ 5/index.php
@@ -0,0 +1,18 @@
+<?php
+function calculateElectricityBill($units) { if ($units <= 50) {
+$bill = $units * 3.50;
+} elseif ($units <= 150) {
+$bill = (50 * 3.50) + (($units - 50) * 4.00);
+} elseif ($units <= 250) {
+$bill = (50 * 3.50) + (100 * 4.00) + (($units - 150) *
+5.20);
+} else {
+$bill = (50 * 3.50) + (100 * 4.00) + (100 * 5.20) +
+(($units - 250) * 6.50);
+}
+return $bill;
+}
+$unitsConsumed = 300;
+$electricityBill = calculateElectricityBill($unitsConsumed); echo "Units Consumed: $unitsConsumed\n";
+echo "Electricity Bill: Rs. $electricityBill\n";
+?>
